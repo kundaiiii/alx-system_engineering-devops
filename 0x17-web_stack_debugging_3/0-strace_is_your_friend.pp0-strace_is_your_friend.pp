20ec364dd14ea1cc610d7e3be93755d2c2f2d8f9
kundaiiii <kundaimm@yahoo.com>
2023-06-21 18:08:25 +0000

    Task + README.md update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+# Puppet script that fixes whole typo errors in php files
+exec { '/var/www/html/wp-setting.php':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
